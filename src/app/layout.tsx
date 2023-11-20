2656e3297971f7bcf574fceb2a2afe8c10f33348
Shakeel Haider <shkhaider2015@gmail.com>
2023-11-20 22:32:57 +0500

    Add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import StyledComponentsRegistry from "@/lib/AntdRegistry";
+import LayoutComp from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <LayoutComp>{children}</LayoutComp>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 import HomeScreen from '@/components/screens/Home'
-import { Button } from 'antd'
 
 export default function Home() {
   return (
