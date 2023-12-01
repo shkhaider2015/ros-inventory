ce20134a745fa52da64611b02016a1fa9a0035f5
Shakeel Haider <shkhaider2015@gmail.com>
2023-12-01 20:31:40 +0500

    checkout screen complete

--- src/components/common/CounterButton/index.tsx
+++ src/components/common/CounterButton/index.tsx
@@ -1,16 +1,34 @@
-"use client"
+"use client";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
-const CounterButton = () => {
+const CounterButton: React.FC<ICounterButton> = (props) => {
+  const { width = 110, value = 1 } = props;
+  const [count, setCount] = useState<number>(value);
+
+  const _increment = () => setCount((pS) => pS + 1);
+  const _decrement = () => setCount((pS) => (pS > 1 ? pS - 1 : 1));
   return (
-    <div className={styles.container} style={{
-        width: 80
-    }} >
-      <div className={styles.btn} >-</div>
-      <div>2</div>
-      <div className={styles.btn} >+</div>
+    <div
+      className={styles.container}
+      style={{
+        width: width,
+      }}
+    >
+      <div className={styles.btn} onClick={_decrement}>
+        -
+      </div>
+      <div>{count}</div>
+      <div className={`${styles.btn} ${styles.plusBtn}`} onClick={_increment}>
+        +
+      </div>
     </div>
   );
 };
+
+interface ICounterButton {
+  width?: string | number;
+  value?: number;
+}
 
 export default CounterButton;
