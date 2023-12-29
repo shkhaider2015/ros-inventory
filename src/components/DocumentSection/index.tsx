b574ce53189edd2b42a1245cb5c3acab9424a92a
Shakeel Haider <shkhaider2015@gmail.com>
2023-12-29 21:42:59 +0500

    add upload api

--- src/components/DocumentSection/index.tsx
+++ src/components/DocumentSection/index.tsx
@@ -4,16 +4,66 @@ import styles from "./styles.module.css";
 import TextEditor from "../TextEditor";
 import Button from "../common/Button";
 import { IAttachements, IInventoryItem } from "@/screens/Home";
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import React, { useState } from "react";
+import Loader from "../common/Loader";
 
 const DocumentSection = (props: {
   item: IInventoryItem | undefined;
   attachements: IAttachements[];
   section_type?: string;
 }) => {
+  const [loading, setLoading] = useState<boolean>(false);
 
-  console.log("File ", props.attachements);
-  
+  const _uploadFileToServer = async (fileData: IUploadData): Promise<any> => {
+    let URL = "https://myapi.runofshowapp.com/api/inventory/uploadFileClient";
+    if(!fileData.workspace_id) return Promise.reject("Workspace_id in undefined")
+    if(!fileData.section_type) return Promise.reject("section_type in undefined")
+    try {
+      let res: AxiosResponse<{ url: string }> = await axios.post(URL, fileData, {
+        headers: {
+          "Content-Type": fileData.file_type,
+        },
+      });
+
+      return Promise.resolve(res.data.url);
+    } catch (error: any) {
+      console.log("Error : ", error);
+
+      let err = null;
+      if (error instanceof AxiosError) err = error.message;
+      else err = error;
+      return Promise.reject(err);
+    }
+  };
+
+  const _handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    let file = e.target.files?.[0];
+    let workspaceId = props.item?.workspace_id
+    let sectionType = props.item?.type;
+
+    if (!file) return;
+    try {
+      setLoading(true);
+      let data:IUploadData = {
+        name: file.name,
+        description: file.name,
+        workspace_id: workspaceId,
+        file_name: `inventory-${workspaceId}-${Date.now()}-file`,
+        file_type: file.type,
+        section_type: sectionType
+      }
+      let s3URL = await _uploadFileToServer(data);
+      console.log("S3URL : ", s3URL);
+    } catch (error) {
+      console.error("_handleUpload Error : ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // console.log("File ", props.attachements);
+
   return (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
@@ -57,34 +107,48 @@ const DocumentSection = (props: {
           ))}
       </div>
       <div className={styles.btnCon}>
-        <div className={styles.btn}>+ Upload Document</div>
+        <label className={styles.btn} htmlFor="upload-file">
+          {loading ? (
+            <Loader />
+          ) : (
+            <input
+              type="file"
+              name="upload-file"
+              id="upload-file"
+              onChange={_handleUpload}
+            />
+          )}
+          + Upload Document
+        </label>
       </div>
     </div>
   );
 };
 
-const DocItem: React.FC<IAttachements> = ({ name, description, url, file_logo }) => {
+const DocItem: React.FC<IAttachements> = ({
+  name,
+  description,
+  url,
+  file_logo,
+}) => {
   const _downloadFile = () => {
-    window.open(url, "_blank");    
+    window.open(url, "_blank");
   };
 
   return (
     <div className={styles.docContainer}>
       <div className={styles.leftCol}>
         <div className={styles.docIconContainer}>
-          {
-            file_logo ? <Image
-            src={file_logo}
-            alt=""
-            width={20}
-            height={20}
-          /> : <Image
-          src={"/images/icons/FileText.svg"}
-          alt=""
-          width={20}
-          height={20}
-        />
-          }
+          {file_logo ? (
+            <Image src={file_logo} alt="" width={20} height={20} />
+          ) : (
+            <Image
+              src={"/images/icons/FileText.svg"}
+              alt=""
+              width={20}
+              height={20}
+            />
+          )}
           {/* <Image
             src={"/images/icons/FileText.svg"}
             alt=""
@@ -112,25 +176,13 @@ const DocItem: React.FC<IAttachements> = ({ name, description, url, file_logo })
   );
 };
 
-
-
-// interface IInventoryItem {
-//   description: string;
-//   icon_url: string | undefined;
-//   id: string;
-//   name: string;
-//   quantity: number;
-//   rental_price: number;
-//   type:
-//     | "INVENTORY_MENU"
-//     | "VENUE_SPEC"
-//     | "KITCHEN_SUPPLY"
-//     | "ABOUT_THE_VENUE"
-//     | "INSURANCE_REQUIREMENTS"
-//     | "FOOD_AND_BEVERAGE"
-//     | "MISC";
-//   workspace_id?: string;
-//   updated_at?: string;
-// }
+interface IUploadData {
+  name: string;
+  file_type: string;
+  file_name: string;
+  workspace_id: string | undefined;
+  description: string;
+  section_type: string | undefined;
+}
 
 export default DocumentSection;
