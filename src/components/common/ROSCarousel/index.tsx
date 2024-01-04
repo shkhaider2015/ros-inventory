fed7b8015f52e507792822f143a9474c231a0b8f
Shakeel Haider <shkhaider2015@gmail.com>
2024-01-04 20:44:33 +0500

    update ROS Causoul

--- src/components/common/ROSCarousel/index.tsx
+++ src/components/common/ROSCarousel/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import styles from "./styles.module.css";
 import Image from "next/image";
@@ -7,24 +7,25 @@ import Image from "next/image";
 const ROSCarousel: React.FC<IROSCarousel> = (props) => {
   const { onClose, open, images } = props;
   const [element, setElement] = useState<Element | null>(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(-1);
 
   // console.log("Images : ", images);
-  
 
   useEffect(() => {
     if (typeof window !== "undefined")
       setElement(document.getElementById("carousel-root"));
   }, []);
 
+  useLayoutEffect(() => {
+    if(open) setCurrentIndex(0);
+    else setCurrentIndex(-1)
+  }, [open])
+
   function _handleClose() {
+    setCurrentIndex(-1)
     onClose();
   }
 
-  const handleImageLoad = () => {
-    setIsImageLoaded(true);
-  };
 
   const content = (
     <div
@@ -34,7 +35,9 @@ const ROSCarousel: React.FC<IROSCarousel> = (props) => {
     >
       <div className={styles.header}>
         <div />
-        <div className={styles.itemCoun} >{`${currentIndex+1}/${ images ? images.length : 0}`}</div>
+        <div className={styles.itemCoun}>{`${currentIndex + 1}/${
+          images ? images.length : 0
+        }`}</div>
         <div className={styles.closeIcon} onClick={_handleClose}>
           <Image
             src={"/images/icons/Close_round.svg"}
@@ -45,14 +48,16 @@ const ROSCarousel: React.FC<IROSCarousel> = (props) => {
         </div>
       </div>
       <div className={styles.carouselContentContainer}>
-      <div className={styles.CMContainer}>
+        <div className={styles.CMContainer}>
           <div className={styles.CMleft}>
             <div
               className={styles.arrowContainer}
-              onClick={() => setCurrentIndex((pS) => {
-                if(pS <= 0) return images.length - 1;
-                return pS - 1;
-              })}
+              onClick={() =>
+                setCurrentIndex((pS) => {
+                  if (pS <= 0) return images.length - 1;
+                  return pS - 1;
+                })
+              }
             >
               <Image
                 src={"/images/icons/Expand_left.svg"}
@@ -63,37 +68,30 @@ const ROSCarousel: React.FC<IROSCarousel> = (props) => {
             </div>
           </div>
           <div className={styles.CMimageSection}>
-            {/* {
-              images && images.length > 0 && <Image
-              className={`${styles.image} ${isImageLoaded ? styles.visible : styles.hidden}`}
-              src={images[currentIndex] || ""}
-              alt=""
-              width={800}
-              height={600}
-              layout="responsive"
-              onLoad={handleImageLoad}
-            />
-            } */}
-             {
-              images && images.length > 0 && <Image
-              className={`${styles.image} ${isImageLoaded ? styles.visible : styles.hidden}`}
-              src={images[currentIndex] || ""}
-              alt=""
-              width={800}
-              height={600}
-              layout="responsive"
-              onLoad={handleImageLoad}
-            />
-            }
-            
+            {images &&
+              images.length &&
+              images.map((item: string, index: number) => (
+                <Image
+                  className={`${styles.image} ${
+                    currentIndex == index ? styles.imageVisible : styles.imageHidden
+                  }`}
+                  src={item}
+                  alt=""
+                  width={800}
+                  height={600}
+                  layout="responsive"
+                />
+              ))}
           </div>
           <div className={styles.CMright}>
             <div
               className={styles.arrowContainer}
-              onClick={() => setCurrentIndex((pS) => {
-                if(pS >= images.length-1) return 0;
-                return pS + 1
-              })}
+              onClick={() =>
+                setCurrentIndex((pS) => {
+                  if (pS >= images.length - 1) return 0;
+                  return pS + 1;
+                })
+              }
             >
               <Image
                 src={"/images/icons/Expand_right.svg"}
@@ -114,7 +112,7 @@ const ROSCarousel: React.FC<IROSCarousel> = (props) => {
 interface IROSCarousel {
   onClose: () => void;
   open: boolean;
-  images: string[]
+  images: string[];
 }
 
 export default ROSCarousel;
