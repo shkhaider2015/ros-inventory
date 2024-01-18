eef383fbb4cde91cb3dbaa06405892a391249ba7
Shakeel Haider <shkhaider2015@gmail.com>
2024-01-18 17:15:20 +0500

    chat integrated successfully

--- src/components/CometChat/index.tsx
+++ src/components/CometChat/index.tsx
@@ -2,6 +2,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 
 const CometChatPopup = (props: { event_id: string }) => {
   const [user, setUser] = useState<any>(null);
@@ -14,7 +15,8 @@ const CometChatPopup = (props: { event_id: string }) => {
     }
   }, []);
 
-  const _loginUser = async () => {
+  const _loginUser = async (call=0) => {
+    if(call > 2) return;
     const CometChatUIKit = (await import("@cometchat/chat-uikit-react"))
       .CometChatUIKit;
     await CometChatUIKit.getLoggedinUser()
@@ -29,11 +31,18 @@ const CometChatPopup = (props: { event_id: string }) => {
             })
             .catch((err) => console.log("Login Error : ", err));
         } else {
+          setUser(res)
           await _getGroup();
         }
       })
       .catch((err: any) => {
         console.log("LogedInUser -> Error : ", err);
+        if(err?.toLowerCase()?.includes("uikitsettings")) {
+          console.log("TRuuuE");
+          setTimeout(async () => {
+            await _loginUser(call + 1)
+          }, 1500);
+        }
       });
   };
 
@@ -71,9 +80,12 @@ const CometChatPopup = (props: { event_id: string }) => {
       >
         <ChatComponent group={group} />
       </div>
-      <div className={styles.chatBtn} onClick={() => setIsVisible(!isVisible)}>
-        btn
+      {
+        user && <div className={styles.chatBtn} onClick={() => setIsVisible(!isVisible)}>
+        <Image src={isVisible ? "/images/icons/Close_round_primary.svg" : "/images/icons/Chat_alt_2.svg"} alt="chat icon" width={25} height={25} />
       </div>
+      }
+      
     </div>
   );
 };
