af08b9dfee975c6a716befe60e7472ce5c47178d
Shakeel Haider <shkhaider2015@gmail.com>
2024-02-06 11:33:02 +0500

    change url to production

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -45,7 +45,7 @@ const Header= () => {
       let event_id = pathName.split('/')?.[2];
       setLoading(true);
       const response = await axios.post(
-        URL,
+        Prod,
         { event_id: event_id},
         { responseType: "blob" }
       );
